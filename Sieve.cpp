c96be0cc6b598471e7e808eef53e8b653ed7b556
tanbeertaher <33250564+tanbeertaher@users.noreply.github.com>
2020-07-01 22:26:27 +0600

    Create Sieve.cpp

---
+++ Sieve.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define M 100000
+
+using namespace std;
+vector<int> primes;
+bitset<M> mark;
+void sieve(int n) {
+    for (int i = 2; i*i <= n; i++) {
+        if (mark[i] == false) { // i is a prime
+            for (int j = i + i; j <= n; j += i ) {
+                mark[j] = true;
+            }
+        }
+    }
+    for (int i = 2; i <= n; i++)
+        if (mark[i] == false)  // i is a prime
+            primes.push_back(i);
+}
+
+bool isPrime(int n) {
+    if (n < 2) return false;
+    return !mark[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    sieve(n);
+    for (int i = 0; i < (int)primes.size(); i++)
+        cout << primes[i] << '\n';
+}
