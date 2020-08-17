e083955de5830c8085b99e18f5ee1ab7796ecb60
tanbeertaher <33250564+tanbeertaher@users.noreply.github.com>
2020-08-17 19:23:27 +0600

    Create Modular Arithmetic.cpp

---
+++ Modular Arithmetic.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n = 1000, mod = 1e9+7;
+    int ans = 1;
+    for(int i = 1; i<=n ; i++)
+        ans = (ans*i)%mod;
+    cout<<ans<<endl;
+}
