de71f01652386f8012b7e2da9af94744c21ac2ef
tanbeertaher <33250564+tanbeertaher@users.noreply.github.com>
2020-08-23 18:09:05 +0600

    Update bigmod.cpp

--- bigmod.cpp
+++ bigmod.cpp
@@ -6,14 +6,14 @@ long int bigmod(int a, int b, int m)
     if (b == 0) return 1;
     if (b % 2 == 0)
     {
-        long int x = mod(a, b / 2, m);
+        long int x = bigmod(a, b / 2, m);
         return (x * x) % m;
     }
-    else return ((long int)a % m * mod(a, b - 1, m)) % m;
+    else return ((long int)a % m * bigmod(a, b - 1, m)) % m;
 }
 
 int main() {
     long int a, b, m;
     while (cin >> a >> b >> m)
-        cout << mod(a, b, m) << endl;
+        cout << bigmod(a, b, m) << endl;
 }
